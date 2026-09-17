fdffa672db995c3d99f522ba842ba1953e40ad5c
romain <romaindenoual1808@gmail.com>
2026-09-16 21:13:17 -0400

    feat: implementation of menuItem class and subclasses

--- src/main/java/model/Beverage.java
+++ src/main/java/model/Beverage.java
@@ -3,23 +3,29 @@ package model;
 import java.math.BigDecimal;
 
 public class Beverage extends MenuItem {
+    private int volumeMl;
+    private boolean alcoholic;
 
     public Beverage(String id, String name, BigDecimal price, Category category, boolean available,
                     int volumeMl, boolean alcoholic) {
         super(id, name, price, category, available);
+        this.volumeMl = volumeMl;
+        this.alcoholic = alcoholic;
     }
 
     public boolean isAlcoholic() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int getPreparationMinutes() {
-        throw new UnsupportedOperationException();
+        return this.alcoholic;
     }
 
     @Override
     public String describe() {
-        throw new UnsupportedOperationException();
+        return "=== Order #" + this.getId() + " ===" +
+            "\n-- " + this.getName() + " --" +
+            "\nPrice: " + this.getPrice() +
+            "\nCategory: " + this.getCategory() +
+            "\nAvailable: " + this.isAvailable() +
+            "\nAlcohol: " + this.alcoholic +
+            "\nVolume (ml): " + this.volumeMl +
+            "\n=========";
     }
 }
