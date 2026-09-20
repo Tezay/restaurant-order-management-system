e518026fa2f7075b7ad12c988a32596eb4471f18
romain <romaindenoual1808@gmail.com>
2026-09-19 23:31:45 -0400

    feat(repository): implement Repository and Menu

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -3,20 +3,36 @@ package repository;
 import contract.Identifiable;
 import exception.RestaurantException;
 
-import java.util.Collection;
-import java.util.Optional;
+import java.util.*;
 
 public class Repository<T extends Identifiable> {
+    private final Map<String, T> items = new TreeMap<>();
 
     public void add(T item) throws RestaurantException {
-        throw new UnsupportedOperationException();
+        if (item == null){
+            throw new RestaurantException("Item cannot be null.");
+        }
+        String id = item.getId();
+        if(id == null || id.isEmpty()){
+            throw new RestaurantException("Item's id cannot be empty.");
+        }
+        if(items.containsKey(item.getId())){
+            throw new RestaurantException("Item's id already exists.");
+        }
+        items.put(item.getId(),item);
     }
 
     public Optional<T> findById(String id) {
-        throw new UnsupportedOperationException();
+        if(id == null){
+            return Optional.empty();
+        }
+        // get() would only return null if no key is absent
+        // ofNullable return an empty Optional instead of null
+        return Optional.ofNullable(items.get(id));
     }
 
     public Collection<T> getAll() {
-        throw new UnsupportedOperationException();
+        // read only
+        return Collections.unmodifiableCollection(items.values());
     }
 }
